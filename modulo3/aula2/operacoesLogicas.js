bd8ba6c9fdc0797958d2c6d030f346ddbc7fd0a9
italosilva02 <italosilv0101@gmail.com>
2021-12-29 15:31:53 -0300

    Operadores Lógicos

---
+++ modulo3/aula2/operacoesLogicas.js
@@ -0,0 +1,22 @@
+/*Operadores Lógicos
+
+&& -> AND -> E -> Todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR -> OU 
+! -> NOT -> NÃO*/ 
+
+const expressao = false && false && false &&true && false;
+const expressao1 = false && false && false &&false && false;
+const expressao2 = false || false || false ||true || false;
+const expressao3 = true || true || false ||false || true;
+const expressao4 = false || false || false ||true || false;
+const expressao5 = false && false && true &&true || false;
+
+console.log(expressao, expressao1, expressao2, expressao3, expressao4, expressao5);
+
+
+const usuario = 'Luiz';
+const senha = '123456';
+
+const vaiLogar = usuario === 'Luiz' && senha === '123456';
+
+console.log('Vai logar: ' + vaiLogar);
